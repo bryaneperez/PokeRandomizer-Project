9fedfa3c808310253a8e1f1ff52f1d0683e33c00
Bryan Perez <bryaneperez@protonmail.com>
2025-01-19 01:48:01 -0500

    Generation happening with bugs, will fix later

--- poke-randomizer/src/app/page.tsx
+++ poke-randomizer/src/app/page.tsx
@@ -3,15 +3,23 @@ import {Button} from "@material-tailwind/react";
 import {SimpleFooter} from "./components/Footer";
 import {Filter} from "./components/Filter";
 import {Display} from "./components/Display"
+import React, { useState } from 'react';
 
-const handleClick = () => {
-  console.log("Test");
-};
+
+
 
 export default function Home() {
+  const [cardKey, setCardKey] = useState<number>(0);
+
+const handleClick = () => setCardKey(prevKey => prevKey + 1);
+
   return <div className="flex-col flex items-center">
-      <Button className="justify-items-center" onClick={handleClick}>Generate Pokemon</Button>
+      <Button className="justify-items-center" onClick={handleClick}>
+        Generate Pokemon
+        </Button>
+      <div key = {cardKey}>
       <Display></Display>
+      </div>
       <div className ="flex min-h-screen">
       <SimpleFooter></SimpleFooter>
       </div>
