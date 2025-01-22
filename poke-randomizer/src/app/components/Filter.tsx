89f5534b7b57e4dbc6cfe9ac42f8b6f08123ae32
Bryan Perez <bryaneperez@protonmail.com>
2025-01-21 20:46:44 -0500

    Working on filters, might have it separated by pages to be easier to implement. Might go for a drop down approach but for now im getting filter arrays set up.

--- poke-randomizer/src/app/components/Filter.tsx
+++ poke-randomizer/src/app/components/Filter.tsx
@@ -1,66 +1,17 @@
-import React from 'react'
-import {
-  Menu,
-  Drawer,
-  Button,
-  Typography,
-  IconButton,
-  MenuHandler,
-  MenuList,
-  MenuItem
-} from "@material-tailwind/react";
+const Gen1Moves :string[] = [];
+const Gen2Moves :string[] = [];
+const Gen3Moves :string[] = [];
+const Gen4Moves :string[] = [];
+const Gen5Moves :string[] = [];
+const Gen6Moves :string[] = [];
+const Gen7Moves :string[] = [];
+const Gen8Moves :string[] = [];
+const Gen9Moves :string[] = [];
 
-
-export function Filter() {
-    const [open, setOpen] = React.useState(false);
-   
-    const openDrawer = () => setOpen(true);
-    const closeDrawer = () => setOpen(false);
-   
-    return (
-      <React.Fragment>
-        <Button onClick={openDrawer}>Filters</Button>
-        <Drawer open={open} onClose={closeDrawer} className="p-4">
-          <div className="mb-6 flex items-center justify-between">
-            <Typography variant="h5" color="blue-gray">
-              Filters
-            </Typography>
-            <IconButton variant="text" color="blue-gray" onClick={closeDrawer}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={2}
-                stroke="currentColor"
-                className="h-5 w-5"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </IconButton>
-          </div>
-          <div className="justify-items-center flex flex-col">
-            <Menu>
-                <MenuHandler>
-                <Button size="sm">Generation</Button>
-                </MenuHandler>
-                <MenuList className='z-[9999]'>
-                    <MenuItem>Generation 1</MenuItem>
-                    <MenuItem>Generation 2</MenuItem>
-                    <MenuItem>Generation 3</MenuItem>
-                    <MenuItem>Generation 4</MenuItem>
-                    <MenuItem>Generation 5</MenuItem>
-                    <MenuItem>Generation 6</MenuItem>
-                    <MenuItem>Generation 7</MenuItem>
-                    <MenuItem>Generation 8</MenuItem>
-                    <MenuItem>Generation 9</MenuItem>
-                </MenuList>
-            </Menu>
-          </div>
-        </Drawer>
-      </React.Fragment>
-    );
-  }
+const Gen3Abilities :string[] = [];
+const Gen4Abilities :string[] = [];
+const Gen5Abilities :string[] = [];
+const Gen6Abilities :string[] = [];
+const Gen7Abilities :string[] = [];
+const Gen8Abilities :string[] = [];
+const Gen9Abilities :string[] = [];
